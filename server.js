b746fdb8f5c657519abc43088a3e094384f24565
Glitch (fcc-reeqheader) <none>
2018-05-28 02:28:00 +0000

    🎠😻 Updated with Glitch

--- server.js
+++ server.js
@@ -13,7 +13,13 @@ app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+  var result = {}
+  //{"ipaddress":"49.255.32.94","language":"en-US","software":"Windows NT 10.0; Win64; x64"}
+  result.ipaddress=request.header('x-forwarded-for').split(',')[0] || request.connection.remoteAddress
+  result.language=request.headers['accept-language'].split(',')[0]
+  result.software=request.headers['user-agent'].split('(')[1].split(')')[0]
+  var resp = JSON.stringify(result)
+  response.send(resp);
 });
 
 // listen for requests :)
